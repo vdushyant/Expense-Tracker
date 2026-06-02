bd4753639af324987f97bb052537d25571271db0
Dushyant Verma <Dushyantverma.cs11@gmail.com>
2026-06-02 21:37:23 +0530

    Configure Swagger JWT bearer authentication

--- src/main/java/com/dushyant/expensetracker/config/OpenApiConfig.java
+++ src/main/java/com/dushyant/expensetracker/config/OpenApiConfig.java
@@ -1,7 +1,10 @@
 package com.dushyant.expensetracker.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,6 +17,16 @@ public class OpenApiConfig {
                 .info(new Info()
                         .title("Expense Tracker API")
                         .description("REST API documentation for Expense Tracker backend application")
-                        .version("1.0.0"));
+                        .version("1.0.0"))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .name("bearerAuth")
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        )
+                );
     }
 }
