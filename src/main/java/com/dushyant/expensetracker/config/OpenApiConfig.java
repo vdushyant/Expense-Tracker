27ae052bc961290a5648888af58bc268025a02b1
Dushyant Verma <Dushyantverma.cs11@gmail.com>
2026-06-02 16:09:19 +0530

    Add Swagger API documentation

---
+++ src/main/java/com/dushyant/expensetracker/config/OpenApiConfig.java
@@ -0,0 +1,19 @@
+package com.dushyant.expensetracker.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI expenseTrackerOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Expense Tracker API")
+                        .description("REST API documentation for Expense Tracker backend application")
+                        .version("1.0.0"));
+    }
+}
